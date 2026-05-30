6045315b8c75f7a64ef344a4969439b5eb94bb66
aktanoli <80654559+aktanoli@users.noreply.github.com>
2026-05-30 15:31:11 +0000

    fix: change extensions to mdx for math rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import remarkMath from 'remark-math';
@@ -7,6 +7,30 @@ import rehypeKatex from 'rehype-katex';
 export default defineConfig({
 	site: 'https://thequantscientist.vercel.app',
 	integrations: [mdx(), sitemap()],
+	fonts: [
+		{
+			name: 'Atkinson',
+			cssVariable: '--font-atkinson',
+			provider: fontProviders.local(),
+			fallbacks: ['sans-serif'],
+			options: {
+				variants: [
+					{
+						src: ['./src/assets/fonts/atkinson-regular.woff'],
+						weight: 400,
+						style: 'normal',
+						display: 'swap',
+					},
+					{
+						src: ['./src/assets/fonts/atkinson-bold.woff'],
+						weight: 700,
+						style: 'normal',
+						display: 'swap',
+					},
+				],
+			},
+		},
+	],
 	markdown: {
 		remarkPlugins: [remarkMath],
 		rehypePlugins: [rehypeKatex],
